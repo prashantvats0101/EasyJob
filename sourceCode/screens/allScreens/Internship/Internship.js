410ab273a8ba13a6ca8d78322d56639a74762879
NDVashist <nikhil.dhiman7973@gmail.com>
2024-08-07 17:43:41 +0530

    add intership screen

---
+++ sourceCode/screens/allScreens/Internship/Internship.js
@@ -0,0 +1,14 @@
+import { StyleSheet, Text, View } from 'react-native'
+import React from 'react'
+
+const Internship = () => {
+  return (
+    <View>
+      <Text>Internship</Text>
+    </View>
+  )
+}
+
+export default Internship
+
+const styles = StyleSheet.create({})
